d96a2c20ea36b615b4c81620e0023311c32e85d6
jessicayin <jessica_yin@outlook.com>
2016-10-08 13:16:21 -0400

    rain safety

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,8 @@
 var vinElem = document.getElementById('vin');
 gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
+  
+var signalFilter = ['wipers_on', 'light_level']
+gm.info.getVehicleData(function(data) {
+  vinElem.innerHTML = gm.info.getVIN();
 });
