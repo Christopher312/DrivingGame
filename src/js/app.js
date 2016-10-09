f8380bd8bde3fc05e4b3d93ae28e321141a811a8
Michael You <youmichaelc@gmail.com>
2016-10-09 04:56:23 -0400

    fixed app.js error message

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@
 var vinElem = document.getElementById('vin');
 gm.info.getVehicleConfiguration(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
-  
+
 var signalFilter = ['wipers_on', 'light_level']
 gm.info.getVehicleData(function(data) {
   vinElem.innerHTML = gm.info.getVIN();
